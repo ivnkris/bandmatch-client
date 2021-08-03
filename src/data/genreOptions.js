685fbfa199736d89e3062c8f52f3a26f5bf0d55b
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-03 21:18:06 +0100

    updated test data values to include id

--- src/data/genreOptions.js
+++ src/data/genreOptions.js
@@ -1,10 +1,10 @@
 const genreOptions = [
-  { value: "rock", label: "Rock" },
-  { value: "pop", label: "Pop" },
-  { value: "jazz", label: "Jazz" },
-  { value: "country", label: "Country" },
-  { value: "blues", label: "Blues" },
-  { value: "hiphop", label: "Hip-hop" },
+  { value: "60ff12ead45f6264a425ff8f", label: "Rock" },
+  { value: "60ff12ead45f6264a425ff90", label: "Pop" },
+  { value: "60ff12ead45f6264a425ff91", label: "Jazz" },
+  { value: "60ff12ead45f6264a425ff92", label: "Country" },
+  { value: "60ff12ead45f6264a425ff93", label: "Blues" },
+  { value: "60ff12ead45f6264a425ff94", label: "Hip-hop" },
 ];
 
 module.exports = genreOptions;
