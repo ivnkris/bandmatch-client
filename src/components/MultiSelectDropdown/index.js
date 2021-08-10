d41746743e75c367d32d77cf108d4235981449e8
Dominika <domi_nika@hotmail.co.uk>
2021-08-10 16:14:42 +0100

    WIP: change dropdown colour to black

--- src/components/MultiSelectDropdown/index.js
+++ src/components/MultiSelectDropdown/index.js
@@ -1,7 +1,15 @@
 import Select from "react-select";
 import { Controller } from "react-hook-form";
+// import chroma from "chroma-js";
 
 import "./MultiSelectDropdown.css";
+
+// const colourStyles = {
+//   option: (styles, { data }) => {
+//     const color = chroma(data.color);
+//     return { ...styles, color: "#131313" };
+//   },
+// };
 
 const MultiSelectDropDown = ({
   options,
@@ -17,23 +25,27 @@ const MultiSelectDropDown = ({
       <div className="question-div">
         <div className="question">{label}</div>
       </div>
-      <Controller
-        render={({ field: { onChange, value, ref } }) => (
-          <Select
-            inputRef={ref}
-            value={options.filter((option) => value.includes(option.value))}
-            onChange={(selectedOptions) =>
-              onChange(selectedOptions.map((option) => option.value))
-            }
-            options={options}
-            isMulti={isMulti}
-            placeholder={placeholder}
-          />
-        )}
-        name={name}
-        defaultValue={defaultValue || ""}
-        control={control}
-      />
+      <div className="black-text">
+        <Controller
+          render={({ field: { onChange, value, ref } }) => (
+            <Select
+              // styles={colourStyles}
+              className="black-text"
+              inputRef={ref}
+              value={options.filter((option) => value.includes(option.value))}
+              onChange={(selectedOptions) =>
+                onChange(selectedOptions.map((option) => option.value))
+              }
+              options={options}
+              isMulti={isMulti}
+              placeholder={placeholder}
+            />
+          )}
+          name={name}
+          defaultValue={defaultValue || ""}
+          control={control}
+        />
+      </div>
     </div>
   );
 };
