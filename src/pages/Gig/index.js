afaead5b80cfa18751fda2cc9e44470827577fb6
Dominika <domi_nika@hotmail.co.uk>
2021-08-16 19:26:35 +0100

    finalised gig page

--- src/pages/Gig/index.js
+++ src/pages/Gig/index.js
@@ -3,6 +3,9 @@ import { useQuery } from "@apollo/client";
 
 import { GIGS } from "../../graphql/queries";
 import { constructGigCards } from "../../utils/constructCards";
+import Header from "../../components/Header";
+import FilterStrip from "../../components/FilterStrip";
+import Button from "../../components/Button";
 
 const Gig = (props) => {
   const { data, loading, error } = useQuery(GIGS);
@@ -17,9 +20,16 @@ const Gig = (props) => {
   }
 
   if (data) {
-    console.log(data.gigs);
-    const gigCards = constructGigCards(data.gigs);
-    return <>{gigCards}</>;
+    return (
+      <div className="results-page-container">
+        <Header className="pt-3" title="Unleash Your Music to the world" />
+        <FilterStrip title="FIND YOUR GIG" />
+        <div className="see-through-background-90 text-align-center">
+          <div className="cards-container">{constructGigCards(data.gigs)}</div>
+          <Button label="LOAD MORE" size="medium" mode="primary" />
+        </div>
+      </div>
+    );
   }
 };
 
