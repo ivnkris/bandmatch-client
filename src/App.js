d44c21a2ae96f599c98c329a7ba9106aa7419376
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-18 13:15:22 +0100

    added graphql express connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Modal } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
@@ -18,52 +18,52 @@ import "./Reset.css";
 import "./App.css";
 
 const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:4000/",
+	uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  return {
-    headers: {
-      ...headers,
-      authorization: user ? `Bearer ${user.token}` : "",
-    },
-  };
+	const user = JSON.parse(localStorage.getItem("user"));
+	return {
+		headers: {
+			...headers,
+			authorization: user ? `Bearer ${user.token}` : "",
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 const AppModal = () => {
-  const { modalState, setModalState } = useModal();
+	const { modalState, setModalState } = useModal();
 
-  const onHideModal = () => {
-    setModalState({ open: false, content: null });
-  };
+	const onHideModal = () => {
+		setModalState({ open: false, content: null });
+	};
 
-  return (
-    <Modal size="lg" show={modalState.open} onHide={onHideModal} centered>
-      {modalState?.content}
-    </Modal>
-  );
+	return (
+		<Modal size="lg" show={modalState.open} onHide={onHideModal} centered>
+			{modalState?.content}
+		</Modal>
+	);
 };
 
 const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <UserProvider>
-        <ModalProvider>
-          <Router>
-            <NavigationBar />
-            <Routes />
-            <AppModal />
-          </Router>
-        </ModalProvider>
-      </UserProvider>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<UserProvider>
+				<ModalProvider>
+					<Router>
+						<NavigationBar />
+						<Routes />
+						<AppModal />
+					</Router>
+				</ModalProvider>
+			</UserProvider>
+		</ApolloProvider>
+	);
 };
 
 export default App;
