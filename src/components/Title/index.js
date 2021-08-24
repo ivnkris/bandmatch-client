f9e1a8aba89ed7c3b5e44c33b00e7ecf31364ef8
Dominika <domi_nika@hotmail.co.uk>
2021-08-24 17:45:43 +0100

    logic for my profile rendering

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,19 @@
+import { RiPencilFill } from "react-icons/ri";
+
 import "./Title.css";
 import "../../App.css";
 
-const Title = ({ text, type }) => {
-  return <div className={`${type}-title `}>{text}</div>;
+const Title = ({ text, type, myProfile, onClick }) => {
+  if (myProfile) {
+    return (
+      <div className={`${type}-title `}>
+        {text} {"  "}
+        <RiPencilFill className="icon" onClick={onClick} />
+      </div>
+    );
+  } else {
+    return <div className={`${type}-title `}>{text}</div>;
+  }
 };
 
 export default Title;
