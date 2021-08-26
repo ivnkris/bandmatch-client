4bc06d89f7ac6dbcf0d73150a267159ae914292e
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-26 18:08:33 +0100

    updated server url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ import "./Reset.css";
 import "./App.css";
 
 const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
+  uri:
+    "https://boiling-headland-30180.herokuapp.com/" ||
+    "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
