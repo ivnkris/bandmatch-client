9fb0459357ca508e1b1ab66a3a9f1a1e8f0a3541
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-26 20:58:53 +0100

    added pending status to gigs

--- src/components/GigCard/index.js
+++ src/components/GigCard/index.js
@@ -6,143 +6,165 @@ import { GIG_PREVIEW } from "../../graphql/queries";
 import Button from "../Button";
 import { CREATE_GIG_REQUEST } from "../../graphql/mutations";
 import { useUserContext } from "../../contexts/UserProvider";
+import Title from "../Title";
 
 const GigCard = (props) => {
-	const { setModalState, modalState } = useModal();
-	const { state } = useUserContext();
-	const postcode = props.postcode.replace(/[+]/g, " ");
-	const date = moment(props.dateTime * 1)
-		.local()
-		.format("DD-MM-YYYY HH:mm");
-	const [gigRequestQuery] = useMutation(CREATE_GIG_REQUEST);
-	const sendGigRequest = async (event) => {
-		await gigRequestQuery({
-			variables: {
-				createGigRequestInput: {
-					id: props.gigId,
-					performer: { musician: state.user.id },
-				},
-			},
-		});
-	};
+  const { setModalState, modalState } = useModal();
+  const { state } = useUserContext();
+  const postcode = props.postcode.replace(/[+]/g, " ");
+  const date = moment(props.dateTime * 1)
+    .local()
+    .format("DD-MM-YYYY HH:mm");
+  const [gigRequestQuery] = useMutation(CREATE_GIG_REQUEST, {
+    onCompleted: (data) => {
+      setModalState({
+        open: true,
+        content: (
+          <>
+            <Modal.Body className="solid-background messaging-modal">
+              <Title text="REQUEST SENT" type="profile" />
+              <div className="text-center pt-3 mx-3">SUCCESS!</div>
+              <div className="text-center pt-4 mx-5">
+                Your request has been sent to the venue owner. They will review
+                your request and contact you if you are successful!
+              </div>
+            </Modal.Body>
+          </>
+        ),
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  const sendGigRequest = async (event) => {
+    await gigRequestQuery({
+      variables: {
+        createGigRequestInput: {
+          id: props.gigId,
+          performer: { musician: state.user.id },
+        },
+      },
+    });
+  };
 
-	const [getGigInfo] = useLazyQuery(GIG_PREVIEW, {
-		fetchPolicy: "network-only",
-		onCompleted: (data) => {
-			if (!modalState.open) {
-				if (!data) {
-					setModalState({
-						open: true,
-						content: (
-							<Modal.Body className="solid-background">
-								<p>
-									Sorry, we couldn't load information about this gig at this
-									time. Please try again later.
-								</p>
-							</Modal.Body>
-						),
-					});
-				} else {
-					const gig = data.gig;
-					const date = moment(gig.dateTime * 1)
-						.local()
-						.format("DD-MM-YYYY HH:mm");
+  const [getGigInfo] = useLazyQuery(GIG_PREVIEW, {
+    fetchPolicy: "network-only",
+    onCompleted: (data) => {
+      if (!modalState.open) {
+        if (!data) {
+          setModalState({
+            open: true,
+            content: (
+              <Modal.Body className="solid-background">
+                <p>
+                  Sorry, we couldn't load information about this gig at this
+                  time. Please try again later.
+                </p>
+              </Modal.Body>
+            ),
+          });
+        } else {
+          const gig = data.gig;
+          const date = moment(gig.dateTime * 1)
+            .local()
+            .format("DD-MM-YYYY HH:mm");
 
-					setModalState({
-						open: true,
-						content: (
-							<>
-								<Modal.Body className="solid-background text-align-center">
-									<div
-										className="profile-preview-image"
-										style={{
-											backgroundImage: "url(" + gig.imageUrl + ")",
-										}}
-									>
-										<div className="image-overlay">
-											<div className="profile-preview-image-overlay-item">
-												<p>{gig.genre.name} </p>
-											</div>
-										</div>
-									</div>
-									<h5 className="title">{gig.title}</h5>
-									<p> {date} </p>
-									<p> @ {gig.venue.name} </p>
-									{/* {change to city} */}
-									<div>{gig.venue.postcode}</div>
-									<p className="p-yellow pb-2 pb-10">PAY: £{gig.fee}</p>
-									<p className="regular-text">{gig.description}</p>
-									<div
-										className="flex-apart profile-preview-icons-container"
-										user={gig.id}
-									>
-										<Button
-											label="REQUEST"
-											onClick={sendGigRequest}
-											size="small"
-											mode="primary"
-										/>
-										<a href={`venues/${gig.venue.id}`}>
-											<Button label="VENUE" size="small" mode="secondary" />
-										</a>
-									</div>
-								</Modal.Body>
-							</>
-						),
-					});
-				}
-			}
-		},
-		onError: () => {
-			setModalState({
-				open: true,
-				content: (
-					<Modal.Body className="solid-background">
-						<p>
-							Sorry, we couldn't load information about this gig at this time.
-							Please try again later.
-						</p>
-					</Modal.Body>
-				),
-			});
-		},
-	});
+          setModalState({
+            open: true,
+            content: (
+              <>
+                <Modal.Body className="solid-background text-align-center">
+                  <div
+                    className="profile-preview-image"
+                    style={{
+                      backgroundImage: "url(" + gig.imageUrl + ")",
+                    }}
+                  >
+                    <div className="image-overlay">
+                      <div className="profile-preview-image-overlay-item">
+                        <p>{gig.genre.name} </p>
+                      </div>
+                    </div>
+                  </div>
+                  <h5 className="title">{gig.title}</h5>
+                  <p> {date} </p>
+                  <p> @ {gig.venue.name} </p>
+                  {/* {change to city} */}
+                  <div>{gig.venue.postcode}</div>
+                  <p className="p-yellow pb-2 pb-10">PAY: £{gig.fee}</p>
+                  <p className="regular-text">{gig.description}</p>
+                  <div
+                    className="flex-apart profile-preview-icons-container"
+                    user={gig.id}
+                  >
+                    <Button
+                      label="REQUEST"
+                      onClick={sendGigRequest}
+                      size="small"
+                      mode="primary"
+                    />
+                    <a href={`venues/${gig.venue.id}`}>
+                      <Button label="VENUE" size="small" mode="secondary" />
+                    </a>
+                  </div>
+                </Modal.Body>
+              </>
+            ),
+          });
+        }
+      }
+    },
+    onError: () => {
+      setModalState({
+        open: true,
+        content: (
+          <Modal.Body className="solid-background">
+            <p>
+              Sorry, we couldn't load information about this gig at this time.
+              Please try again later.
+            </p>
+          </Modal.Body>
+        ),
+      });
+    },
+  });
 
-	return (
-		<div className="card-container card-container-shortened">
-			<div
-				className="card-image"
-				style={{
-					backgroundImage: "url(" + props.imageUrl + ")",
-				}}
-			>
-				<div className="image-overlay">
-					<div className="card-image-overlay-item">{props.genre}</div>
-				</div>
-			</div>
-			<div>
-				<h3 className="title text-limit-one-line"> {props.title} </h3>
-				<p> {date} </p>
-				<p className="text-limit-two-line">
-					{/* {change to city} */}@ {props.venueName} {postcode}
-				</p>
-			</div>
-			<div id={props.gigId}>
-				<Button
-					label="REQUEST"
-					onClick={sendGigRequest}
-					size="medium"
-					mode="primary"
-				/>
-				<Button
-					label="MORE INFO"
-					size="medium"
-					mode="secondary"
-					onClick={() => getGigInfo({ variables: { gigId: props.gigId } })}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="card-container card-container-shortened">
+      <div
+        className="card-image"
+        style={{
+          backgroundImage: "url(" + props.imageUrl + ")",
+        }}
+      >
+        <div className="image-overlay">
+          <div className="card-image-overlay-item">{props.genre}</div>
+        </div>
+      </div>
+      <div>
+        <h3 className="title text-limit-one-line"> {props.title} </h3>
+        <p> {date} </p>
+        <p className="text-limit-two-line">
+          {/* {change to city} */}@ {props.venueName} {postcode}
+        </p>
+      </div>
+      <div id={props.gigId}>
+        <Button
+          label="REQUEST"
+          onClick={sendGigRequest}
+          size="small"
+          mode="primary"
+        />
+        <Button
+          label="MORE INFO"
+          size="small"
+          mode="secondary"
+          onClick={() => getGigInfo({ variables: { gigId: props.gigId } })}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GigCard;
--- src/components/RequestCard/index.js
+++ src/components/RequestCard/index.js
@@ -3,75 +3,74 @@ import "./RequestCard.css";
 import moment from "moment";
 import { useMutation } from "@apollo/client";
 import { UPDATE_GIG_REQUEST } from "../../graphql/mutations";
-import { findAllByDisplayValue } from "@testing-library/react";
 
 const RequestCard = ({
-	firstName,
-	lastName,
-	name,
-	gigName,
-	imageUrl,
-	musicianId,
-	bandId,
-	gigId,
-	dateTime,
-	id,
+  firstName,
+  lastName,
+  name,
+  gigName,
+  imageUrl,
+  musicianId,
+  bandId,
+  gigId,
+  dateTime,
+  id,
 }) => {
-	const [updateGigRequest] = useMutation(UPDATE_GIG_REQUEST);
-	const acceptRequest = async (event) => {
-		const id = event.target.parentElement.dataset.id;
-		await updateGigRequest({
-			variables: { updateGigRequestInput: { id, confirmed: true } },
-		});
-	};
-	const rejectRequest = async (event) => {
-		const id = event.target.parentElement.dataset.id;
-		await updateGigRequest({
-			variables: {
-				updateGigRequestInput: { id, confirmed: findAllByDisplayValue },
-			},
-		});
-	};
-	return (
-		<div className="musician-request-card solid-background d-flex align-items-center">
-			<div className="musician-image-container">
-				<a href={bandId ? `/bands/${bandId}` : `/profile/${musicianId}`}>
-					<img
-						className="musician-request-image"
-						src={imageUrl}
-						alt={bandId ? name : firstName}
-					/>
-				</a>
-			</div>
-			<div className="musician-info-container text-center">
-				<p>{bandId ? name : firstName + " " + lastName}</p>
-				<p>{gigName}</p>
-				<p>
-					{moment(dateTime * 1)
-						.local()
-						.format("DD-MM-YYYY HH:mm")}
-				</p>
-			</div>
-			<div className="musician-button-container">
-				<div className="musician-button" data-id={id}>
-					<Button
-						mode="primary"
-						size="small"
-						label="ACCEPT"
-						onClick={acceptRequest}
-					/>
-				</div>
-				<div className="musician-button" data-id={id}>
-					<Button
-						mode="secondary"
-						size="small"
-						label="REJECT"
-						onClick={rejectRequest}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  const [updateGigRequest] = useMutation(UPDATE_GIG_REQUEST);
+  const acceptRequest = async (event) => {
+    const id = event.target.parentElement.dataset.id;
+    await updateGigRequest({
+      variables: { updateGigRequestInput: { id, confirmed: "true" } },
+    });
+  };
+  const rejectRequest = async (event) => {
+    const id = event.target.parentElement.dataset.id;
+    await updateGigRequest({
+      variables: {
+        updateGigRequestInput: { id, confirmed: "false" },
+      },
+    });
+  };
+  return (
+    <div className="musician-request-card solid-background d-flex align-items-center">
+      <div className="musician-image-container">
+        <a href={bandId ? `/bands/${bandId}` : `/profile/${musicianId}`}>
+          <img
+            className="musician-request-image"
+            src={imageUrl}
+            alt={bandId ? name : firstName}
+          />
+        </a>
+      </div>
+      <div className="musician-info-container text-center">
+        <p>{bandId ? name : firstName + " " + lastName}</p>
+        <p>{gigName}</p>
+        <p>
+          {moment(dateTime * 1)
+            .local()
+            .format("DD-MM-YYYY HH:mm")}
+        </p>
+      </div>
+      <div className="musician-button-container">
+        <div className="musician-button" data-id={id}>
+          <Button
+            mode="primary"
+            size="small"
+            label="ACCEPT"
+            onClick={acceptRequest}
+          />
+        </div>
+        <div className="musician-button" data-id={id}>
+          <Button
+            mode="secondary"
+            size="small"
+            label="REJECT"
+            onClick={rejectRequest}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RequestCard;
