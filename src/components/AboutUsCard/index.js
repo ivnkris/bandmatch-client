06e622a3daa981c979de3f63aac70c7b78927de2
Soumeya <soumeyahassan47@gmail.com>
2021-08-31 19:39:51 +0100

    about us page started with components

---
+++ src/components/AboutUsCard/index.js
@@ -0,0 +1,55 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { FaUser } from "react-icons/fa";
+
+import { Modal } from "react-bootstrap";
+import "./AboutUsCard.css";
+
+import { useModal } from "../../contexts/ModalProvider";
+
+const AboutUsCard = (props) => {
+  const { setModalState, modalState } = useModal();
+
+  const displayInfo = (description) => {
+    setModalState({
+      open: true,
+      content: (
+        <Modal.Body className="solid-background">
+          <div
+            className="profile-preview-image"
+            style={{
+              backgroundImage: "url(" + props.imageUrl + ")",
+              backgroundPosition: "center center",
+            }}
+          ></div>
+          <p>{description}</p>
+        </Modal.Body>
+      ),
+    });
+  };
+
+  return (
+    <div className="card-container card-container-shortest mx-3">
+      <div
+        className="card-image"
+        style={{
+          backgroundImage: "url(" + props.imageUrl + ")",
+          backgroundPosition: "center center",
+        }}
+      ></div>
+      <div>
+        <h3 className="title text-limit-one-line"> {props.name} </h3>
+      </div>
+      <div className="icons-container">
+        <a href={props.githubUrl} target="_blank" rel="noopener noreferrer">
+          <AiFillGithub size={30} />
+        </a>
+        <a href={props.linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <AiFillLinkedin size={30} />
+        </a>
+        <FaUser size={30} onClick={() => displayInfo(props.description)} />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsCard;
---
+++ src/pages/AboutUs/index.js
@@ -0,0 +1,52 @@
+import AboutUsCard from "../../components/AboutUsCard";
+
+import photo from "../../alexis-baydoun-bAHwQEJqAb8-unsplash.jpg";
+
+const AboutUs = () => {
+  const ourInfo = [
+    {
+      name: "Dominika",
+      imageUrl: photo,
+      description: "our long bio about ourselves goes here",
+      githubUrl: "https://github.com/dominikacookies",
+      linkedinUrl: "https://www.linkedin.com/in/dominika-pietrzak-183755137/",
+    },
+    {
+      name: "Kris",
+      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
+      description: "our long bio about ourselves goes here",
+      githubUrl: "https://github.com/ivnkris",
+      linkedinUrl: "https://www.linkedin.com/in/krisztian-ivan-10880478//",
+    },
+    {
+      name: "Natasha",
+      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
+      description: "our long bio about ourselves goes here",
+      githubUrl: "https://github.com/natasha-mann",
+      linkedinUrl: "https://www.linkedin.com/in/natashamann2896//",
+    },
+    {
+      name: "Soumeya",
+      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
+      description: "our long bio about ourselves goes here",
+      githubUrl: "https://github.com/SoumeyaH",
+      linkedinUrl: "https://www.linkedin.com/in/soumeya-hassan-0a12a5203/",
+    },
+  ];
+
+  const cardsToRender = ourInfo.map((info) => {
+    return (
+      <AboutUsCard
+        key={info.name}
+        name={info.name}
+        imageUrl={info.imageUrl}
+        description={info.description}
+        githubUrl={info.githubUrl}
+        linkedinUrl={info.linkedinUrl}
+      />
+    );
+  });
+  return <div className="results-page-container">{cardsToRender}</div>;
+};
+
+export default AboutUs;
