4b2044ce50dd3c2725dadf77af2acd7482e1e9d0
Soumeya <soumeyahassan47@gmail.com>
2021-09-01 18:58:27 +0100

    added images, and nav link

--- src/components/AboutUsCard/index.js
+++ src/components/AboutUsCard/index.js
@@ -1,36 +1,13 @@
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { FaUser } from "react-icons/fa";
+import { MdWeb } from "react-icons/md";
 
-import { Modal } from "react-bootstrap";
 import "./AboutUsCard.css";
 
-import { useModal } from "../../contexts/ModalProvider";
-
 const AboutUsCard = (props) => {
-  const { setModalState, modalState } = useModal();
-
-  const displayInfo = (description) => {
-    setModalState({
-      open: true,
-      content: (
-        <Modal.Body className="solid-background">
-          <div
-            className="profile-preview-image"
-            style={{
-              backgroundImage: "url(" + props.imageUrl + ")",
-              backgroundPosition: "center center",
-            }}
-          ></div>
-          <p>{description}</p>
-        </Modal.Body>
-      ),
-    });
-  };
-
   return (
-    <div className="card-container card-container-shortest mx-3 card-width">
+    <div className="card-container card-container-extended mx-3 about-card">
       <div
-        className="card-image"
+        className="about-card-image"
         style={{
           backgroundImage: "url(" + props.imageUrl + ")",
           backgroundPosition: "center center",
@@ -40,14 +17,16 @@ const AboutUsCard = (props) => {
         <h3 className="title text-limit-one-line my-3"> {props.name} </h3>
         <p className="text-limit-one-line my-3"> {props.description} </p>
       </div>
-      <div className="icons-container my-3">
+      <div className="about-icons-container my-3">
         <a href={props.githubUrl} target="_blank" rel="noopener noreferrer">
           <AiFillGithub size={30} />
         </a>
         <a href={props.linkedinUrl} target="_blank" rel="noopener noreferrer">
           <AiFillLinkedin size={30} />
         </a>
-        <FaUser size={30} onClick={() => displayInfo(props.description)} />
+        <a href={props.portfolioUrl} target="_blank" rel="noopener noreferrer">
+          <MdWeb size={30} />
+        </a>
       </div>
     </div>
   );
--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -2,37 +2,40 @@ import AboutUsCard from "../../components/AboutUsCard";
 
 import "./AboutUs.css";
 
-import photo from "../../images/alexis-baydoun-bAHwQEJqAb8-unsplash.jpg";
+import tashaPhoto from "../../images/natasha.jpeg";
+import soumeyaPhoto from "../../images/soumeya.jpeg";
+import krisPhoto from "../../images/kris.jpg";
+import domPhoto from "../../images/dom.jpg";
 
 const AboutUs = () => {
   const ourInfo = [
     {
       name: "Dominika",
-      imageUrl: photo,
-      description: "our long bio about ourselves goes here",
+      imageUrl: domPhoto,
       githubUrl: "https://github.com/dominikacookies",
       linkedinUrl: "https://www.linkedin.com/in/dominika-pietrzak-183755137/",
+      portfolioUrl: "https://dominikacookies.github.io/react-portfolio/",
     },
     {
       name: "Kris",
-      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
-      description: "our long bio about ourselves goes here",
+      imageUrl: krisPhoto,
       githubUrl: "https://github.com/ivnkris",
       linkedinUrl: "https://www.linkedin.com/in/krisztian-ivan-10880478//",
+      portfolioUrl: "https://ivnkris.github.io/personal-portfolio-react/#/",
     },
     {
       name: "Natasha",
-      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
-      description: "our long bio about ourselves goes here",
+      imageUrl: tashaPhoto,
       githubUrl: "https://github.com/natasha-mann",
       linkedinUrl: "https://www.linkedin.com/in/natashamann2896//",
+      portfolioUrl: "https://natasha-mann.github.io/portfolio-react/",
     },
     {
       name: "Soumeya",
-      imageUrl: "https://unsplash.com/photos/2L-0vnCnzcU",
-      description: "our long bio about ourselves goes here",
+      imageUrl: soumeyaPhoto,
       githubUrl: "https://github.com/SoumeyaH",
       linkedinUrl: "https://www.linkedin.com/in/soumeya-hassan-0a12a5203/",
+      portfolioUrl: "https://soumeyah.github.io/react-portfolio/",
     },
   ];
 
@@ -45,6 +48,7 @@ const AboutUs = () => {
         description={info.description}
         githubUrl={info.githubUrl}
         linkedinUrl={info.linkedinUrl}
+        portfolioUrl={info.portfolioUrl}
       />
     );
   });
