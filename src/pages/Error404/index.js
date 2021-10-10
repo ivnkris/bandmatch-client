522bf84b6ad386a02557495b23027ae870c0912d
Natasha Mann <natasha.s.mann@gmail.com>
2021-10-10 17:52:08 +0100

    update

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -11,7 +11,7 @@ const Error404 = () => {
           </h1>
         </div>
         <h4 className="my-5 ms-3">
-          Try going back to the previous page or clicking below to go home.
+          Try going back to the previous page or click below to go home.
         </h4>
         <a href="/">
           <Button label="Home" size="medium" mode="primary" />
